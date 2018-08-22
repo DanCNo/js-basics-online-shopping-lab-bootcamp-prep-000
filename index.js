235251fdd9c421bd723ac8dd7c37e869c54d8cef
Cheol Noh <dan.noh@gmail.com>
2018-08-22 19:47:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,12 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if(cardNumber > 0 && Number.isInteger(cardNumber) && ) {
+  if(cardNumber > 0 && Number.isInteger(cardNumber) && cardNumber.toString().length === 8) {
+    //var total = total();
+    cart = [];
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
     
+  } else {
+    return "Sorry, we don't have a credit card on file for you.";
   }
 }
